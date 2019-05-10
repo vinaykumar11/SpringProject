a2f4c583cd1e411c99d688e720c22fb242c34684
vinaykumar11 <vinayboosa@gmail.com>
2019-05-10 17:54:50 -0500

    Spring Boot Application

---
+++ todo-management-spring-boot/src/main/java/net/guides/springboot/todomanagement/TodoManagementSpringBootApplication.java
@@ -0,0 +1,13 @@
+package net.guides.springboot.todomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementSpringBootApplication.class, args);
+	}
+
+}
---
+++ todo-management-spring-boot/src/test/java/net/guides/todo/todomanagementspringboot/TodoManagementSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package net.guides.todo.todomanagementspringboot;
+
+/*import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoManagementSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}*/
